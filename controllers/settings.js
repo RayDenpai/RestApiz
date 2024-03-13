19b524829c8902d7463488b314525c6af1349629
Rayy444 <rayy9721@gmail.com>
2024-03-13 15:32:10 +0700

    V1

---
+++ controllers/settings.js
@@ -0,0 +1,13 @@
+global.author = "Denpai";
+global.key_free = ["freeapikey"];
+global.key_premium = ["denpaigns"];
+global.limit_free = "30";
+global.limit_premium = "99999";
+global.dateLimit = "Oktober, 31 2025",
+global.mongo_Db = "mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority";
+global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
+global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
+global.my_email = "raycs9012@gmail.com";
+global.my_email_password = "RayTodzz123";
+global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
+global.port = 8080
